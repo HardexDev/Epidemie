e7380fc54af7a00b8c416ac60084376283d6f21f
BuildTools <unconfigured@null.spigotmc.org>
2020-11-24 16:21:33 +0100

    Ajout classe euler explicite

---
+++ src/epidemie/EulerExplicite.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package epidemie;
+
+/**
+ *
+ * @author Alexis Robin
+ */
+public class EulerExplicite {
+    private int P = 10000;
+    private double k = 0.0001;
+    private double h = 0.01;
+    private int I0 = 200;
+    
+    public void printResult(int stop){
+        int start = I0;
+    }
+}
